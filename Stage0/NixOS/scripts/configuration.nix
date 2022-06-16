c7d36d72011104c9e47815ae8690589ec6219519
jburnett <jburnett@altamodatech.com>
2022-06-15 20:30:25 -0400

    Break into multiple stages

---
+++ Stage0/NixOS/scripts/configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./hardware-builder.nix
+      ./vagrant.nix
+      ./ansible.nix
+      ./iohk.nix
+      ./custom-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  # remove the fsck that runs at startup. It will always fail to run, stopping
+  # your boot until you press *.
+  boot.initrd.checkJournalingFS = false;
+
+  # Services to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable DBus
+  services.dbus.enable    = true;
+
+  # Replace ntpd by timesyncd
+  services.timesyncd.enable = true;
+
+  security.sudo.extraConfig =
+    ''
+      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+      Defaults:root,%wheel env_keep+=NIX_PATH
+      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+      Defaults env_keep+=SSH_AUTH_SOCK
+      Defaults lecture = never
+      root   ALL=(ALL) SETENV: ALL
+      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+    '';
+
+}
